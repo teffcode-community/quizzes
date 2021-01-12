f6e11a55f3e12e86d1c65989c8f582bd99c73c7a
Estefany Aguilar <tefy.1@live.com>
2021-01-12 15:27:13 -0500

    🐨 Add: Cards container children from quizzes data

--- quizzes.js
+++ quizzes.js
@@ -1,15 +1,10 @@
-class Quizzes {
-  constructor() {
-    this.quizzes = [
-      {
-        technology: "JavaScript",
-        name: "Objects",
-        date: "1 Jan. 2021",
-        url: "/",
-        imageUrl: "https://images.pexels.com/photos/5601091/pexels-photo-5601091.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-      }
-    ]
+import { quizzes } from "./utils.js"
 
+class Quizzes {
+  static JAVASCRIPT = "JavaScript"
+  static CSS = "CSS"
+
+  constructor() {
     this.cardsContainer = document.querySelector(".quizzes-main")
     this.card = document.createElement("a")
     this.image = document.createElement("img")
@@ -22,14 +17,27 @@ class Quizzes {
   }
 
   createCards() {
-    this.quizzes.map((quiz, index) => {
-      this.cardsContainer.appendChild(this.card)
-      this.cardContainer = this.cardsContainer.getElementsByTagName("a")[index]
-      this.cardContainer.appendChild(this.image).setAttribute("src", quiz.imageUrl)
-      this.cardContainer.appendChild(this.color)
-      this.cardContainer.appendChild(this.label).innerHTML = quiz.technology
-      this.cardContainer.appendChild(this.title).innerHTML = quiz.name
-      this.cardContainer.appendChild(this.date).innerHTML = quiz.date
+    quizzes.forEach(quiz => {
+      this.cardContainer = this.cardsContainer.appendChild(this.card.cloneNode(true))
+      this.cardContainer.setAttribute("href", quiz.url)
+      this.cardContainer.setAttribute("target", "_blank")
+      this.cardContainer.appendChild(this.image.cloneNode(true)).setAttribute("src", quiz.imageUrl)
+      this.cardContainer.appendChild(this.image.cloneNode(true)).setAttribute("alt", quiz.imageAlt)
+      this.cardContainer.appendChild(this.label.cloneNode(true)).innerHTML = quiz.technology
+      this.cardContainer.appendChild(this.title.cloneNode(true)).innerHTML = quiz.name
+      this.cardContainer.appendChild(this.date.cloneNode(true)).innerHTML = quiz.date
+
+      let color = this.cardContainer.appendChild(this.color.cloneNode(true))
+
+      if (quiz.technology === Quizzes.JAVASCRIPT) {
+        color.classList.remove("css-color")
+        color.classList.add("javascript-color")
+      }
+
+      if (quiz.technology === Quizzes.CSS) {
+        color.classList.remove("javascript-color")
+        color.classList.add("css-color")
+      }
     })
   }
 }
