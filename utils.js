5aadf98abbdc2401ca9e53a7ea1808014c768925
Estefany Aguilar <tefy.1@live.com>
2021-01-12 15:28:34 -0500

    🦊 Add: Three quizzes data

---
+++ utils.js
@@ -0,0 +1,31 @@
+const JAVASCRIPT = "JavaScript"
+const CSS = "CSS"
+
+const quizzes = [
+  {
+    technology: JAVASCRIPT,
+    name: "Objectsssss",
+    date: "1 Jan. 2021",
+    url: "/",
+    imageUrl: "https://images.pexels.com/photos/5601091/pexels-photo-5601091.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    imageAlt: "Test"
+  },
+  {
+    technology: JAVASCRIPT,
+    name: "Closures",
+    date: "1 Jan. 2021",
+    url: "/",
+    imageUrl: "https://images.pexels.com/photos/5601091/pexels-photo-5601091.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    imageAlt: "Test"
+  },
+  {
+    technology: CSS,
+    name: "Specificity",
+    date: "1 Jan. 2021",
+    url: "/",
+    imageUrl: "https://images.pexels.com/photos/5601091/pexels-photo-5601091.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    imageAlt: "Test"
+  }
+]
+
+export { quizzes }
