4e0ee45fba850c2bdf7a992813d4c5c1c1b083e0
Estefany Aguilar <tefy.1@live.com>
2021-01-30 18:46:19 -0500

    🐨 Add: More than one attribute to an HTML element

--- quizzes.js
+++ quizzes.js
@@ -5,7 +5,6 @@ class Quizzes {
     this.cardsContainer = document.querySelector(".quizzes-main")
     this.card = document.createElement("a")
     this.image = document.createElement("img")
-    this.color = document.createElement("div")
     this.label = document.createElement("p")
     this.title = document.createElement("h2")
     this.date = document.createElement("span")
@@ -15,26 +14,29 @@ class Quizzes {
 
     this.createCards()
   }
+  
+  setAttributes(el, attrs) {
+    for(var key in attrs) {
+      el.setAttribute(key, attrs[key]);
+    }
+  }
 
   createCards() {
     quizzes.forEach(quiz => {
       this.cardContainer = this.cardsContainer.appendChild(this.card.cloneNode(true))
       this.cardContainer.setAttribute("href", quiz.url)
       this.cardContainer.setAttribute("target", "_blank")
-      this.cardContainer.appendChild(this.image.cloneNode(true)).setAttribute("src", quiz.imageUrl)
-      this.cardContainer.appendChild(this.image.cloneNode(true)).setAttribute("alt", quiz.imageAlt)
+      this.setAttributes(this.cardContainer.appendChild(this.image.cloneNode(true)), {"src": quiz.imageUrl, "alt": quiz.imageAlt})
       this.cardContainer.appendChild(this.label.cloneNode(true)).innerHTML = quiz.technology
       this.cardContainer.appendChild(this.title.cloneNode(true)).innerHTML = quiz.name
       this.cardContainer.appendChild(this.date.cloneNode(true)).innerHTML = quiz.date
 
-      let color = this.cardContainer.appendChild(this.color.cloneNode(true))
-
       if (quiz.technology === this.javascript) {
-        color.classList.remove("css-color")
-        color.classList.add("javascript-color")
+        this.cardContainer.getElementsByTagName("p")[0].style.backgroundColor = "#FDFFBC"
+      } else if (quiz.technology === this.css) {
+        this.cardContainer.getElementsByTagName("p")[0].style.backgroundColor = "#FFE3DE"
       } else {
-        color.classList.remove("javascript-color")
-        color.classList.add("css-color")
+        this.cardContainer.getElementsByTagName("p")[0].style.backgroundColor = "#C6FCED"
       }
     })
   }
